a76eb072fdbef1cd6ed81566d0d7c81912c5de51
nbngoc123 <nbngoc.udu.ptit@gmail.com>
2025-10-27 21:28:55 +0700

    'update'

--- app/src/main/java/com/example/audio2text/ui/main/MainActivity.java
+++ app/src/main/java/com/example/audio2text/ui/main/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.audio2text;
+package com.example.audio2text.ui.main;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.example.audio2text.R;
 
 public class MainActivity extends AppCompatActivity {
 
