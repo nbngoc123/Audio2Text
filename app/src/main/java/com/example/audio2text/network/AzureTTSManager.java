5a38b3b83561cb9b83d787e2c67db10b7d53abc5
nbngoc123 <nbngoc.udu.ptit@gmail.com>
2025-12-19 15:59:07 +0700

    add

--- app/src/main/java/com/example/audio2text/network/AzureTTSManager.java
+++ app/src/main/java/com/example/audio2text/network/AzureTTSManager.java
@@ -11,7 +11,7 @@ import java.io.FileOutputStream;
 
 public class AzureTTSManager {
     private SpeechConfig speechConfig;
-    private static final String AZURE_KEY = "6lvGCEMOjWwlR86iys8qPSd504eRNbMYoJD1lKfbkWnLvvt232O0JQQJ99BLAC3pKaRXJ3w3AAAYACOGm86L";
+    private static final String AZURE_KEY = "";
     private static final String AZURE_REGION = "eastasia";
 
     public AzureTTSManager() {
